0cbbf658e29e3276cb598a075b01a5c576521070
artur.dzidzoiev <artur.dzidzoiev@gmail.com>
2016-02-29 00:35:56 +0200

    defaults

---
+++ dataset/fetcher/src/main/java/com/pathfinder/backendless/Defaults.java
@@ -0,0 +1,10 @@
+package com.pathfinder.backendless;
+
+/**
+ * Created by dzidzoiev on 2/29/16.
+ */
+public class Defaults {
+    public static final String APPLICATION_ID = "*";
+    public static final String SECRET_KEY = "*";
+    public static final String VERSION = "*";
+}
